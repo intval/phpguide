9bff7d0394aa37db9124e4585a5dbc7df531ef1c
intval <alex@phpguide.co.il>
2011-08-12 19:44:38 +0400

    Removed meetup right side button

--- protected/views/layouts/footer.php
+++ protected/views/layouts/footer.php
@@ -4,11 +4,6 @@
    שפר את האתר
 </a>
 
-<a class="ideator" style="margin-top:30px;"
-   href="http://phpguide.co.il/%D7%9B%D7%A0%D7%A1+%D7%9E%D7%A4%D7%AA%D7%97%D7%99+PHP+%D7%A8%D7%90%D7%A9%D7%95%D7%9F.htm">
- כנס מפתחים
-</a>
-
 <script type='text/javascript'>
     var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-18788673-3']); _gaq.push(['_trackPageview']);
    // Google analytics can wait to complete page load
