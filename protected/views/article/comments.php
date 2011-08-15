1403824d0ddad5253cd6e9591574f13625554af6
BBLN <doomclanserv@gmail.com>
2011-08-15 10:38:03 +0300

    <a name=""> --> <a id="">

--- protected/views/article/comments.php
+++ protected/views/article/comments.php
@@ -3,7 +3,7 @@
     
 	<h2>תגובות לכתבה:</h2>
         <? foreach($comments as $comment): ?>
-        <a name="comment<?=$comment->cid?>"></a>
+        <a id="comment<?=$comment->cid?>"></a>
         <div class="blog-comment">
             <span class='comment-author'><?=e($comment->author)?></span><span dir="rtl">&nbsp;</span>
             <span dir="ltr" class='comment-date'><?$dt = new DateTime($comment->date); echo $dt->format('d/m/Y H:i')?></span><br/>
