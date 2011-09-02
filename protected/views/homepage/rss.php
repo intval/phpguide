31a7be6587978a7746fc8e16dc353f6030b4b422
intval <Alex@phpguide.co.il>
2011-09-02 17:47:51 +0300

    Added images to rss

--- protected/views/homepage/rss.php
+++ protected/views/homepage/rss.php
@@ -16,12 +16,17 @@
     <? foreach( $articles as $item ): ?>
         <item>
             <title><![CDATA[<?=e($item->title)?>]]></title>
-            <guid><?=bu(urlencode($item->url . '.htm'), true)?></guid>
-            <link><?=bu(urlencode($item->url . '.htm'), true)?></link>
+            <guid><?=e(bu(urlencode($item->url . '.htm'), true))?></guid>
+            <link><?=e(bu(urlencode($item->url . '.htm'), true))?></link>
             <description><![CDATA[
                 <div dir='rtl'>
-
-                    <?= $item->html_desc_paragraph ?>
+                    <table>
+                        <tr>
+                            <td><img src="<?=e($item->image)?>" alt="image"/></td>
+                            <td width="20"></td>
+                            <td valign="top"> <?= $item->html_desc_paragraph ?> </td>
+                        </tr>
+                    </table>
 
                 </div>]]></description>
             <pubDate><?=Helpers::date2rfc(new DateTime($item->pub_date))?></pubDate>
