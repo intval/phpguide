e4e096ed857b193302a7690ec31b9f2995d56b76
intval <alex@phpguide.co.il>
2011-10-29 22:03:23 +0200

    Login controller better error handling (wrong password / username taken)

--- protected/controllers/LoginController.php
+++ protected/controllers/LoginController.php
@@ -2,75 +2,69 @@
 
 class LoginController extends Controller
 {
-	public function actionIndex()
-	{
-            if(isset($_GET['redir'])) 
-            {
-                $return_location = urldecode($_GET['redir']);
-            }
-            else
-            {
-                $return_location = "index.php";
-            }
-            
-			$this->addscripts('ui');
-			
-            $this->render('//homepage/loginpage', array('return_location' => $return_location));
-	}
+    public function actionIndex()
+    {
+        $return_location = isset($_GET['redir']) ? urldecode($_GET['redir']) : Yii::app()->homeUrl ;
+        $this->addscripts('ui');
+        $this->render('//homepage/loginpage', array('return_location' => $return_location));
+    }
 
 
-        public function actionLogin()
+    public function actionLogin()
+    {
+        $result = User::authorize_by_login_and_pass($_POST['login'], $_POST['pass']);
+
+        if( $result instanceof User )
         {
-            $result = User::authorize_by_login_and_pass($_POST['login'], $_POST['pass']);
-    
-            if( $result instanceof User )    
-            {
-                echo('ok'); 
-            }
-            else if( $result == 'locked')        
-            { 
-                echo('ביצעתם יותר מדי נסניונות התחברות. נסו שוב בעוד שעה'); 
-            }
-            else if( $result === null )          
-            { 
-                echo('שם משתמש או סיסמה שגויים'); 
-            }
-            else
-            {
-                echo('System error');
-            }
+            echo('ok');
         }
-        
-        public function actionLogout()
+        else if( $result == 'locked')
         {
-                User::get_current_user()->logout();
-                $this->redirect(Yii::app()->homeUrl);
+            echo('ביצעתם יותר מדי נסניונות התחברות. נסו שוב בעוד שעה');
         }
-        
-        public function actionRegister()
+        else if( $result === null )
         {
-            if(mb_strlen($_POST['user']) > 15)
+            echo('שם משתמש או סיסמה שגויים');
+        }
+        else
+        {
+            echo('System error');
+        }
+    }
+
+    public function actionLogout()
+    {
+            User::get_current_user()->logout();
+            $this->redirect(Yii::app()->homeUrl);
+    }
+
+    public function actionRegister()
+    {
+        if(mb_strlen($_POST['user']) > 15)
+        {
+            echo ':err:' , 'אנה בחרו שם משתמש קצר יותר';
+        }
+        else if( empty($_POST['user']) || empty($_POST['pass']))
+        {
+            echo ':err:' , 'לא הוזנו שם משתמש וסיסמה';
+        }
+        else
+        {
+            try
             {
-                echo ':err:' , 'אנה בחרו שם משתמש קצר יותר';
-            }
-            else if(!empty($_POST['user']) and !empty($_POST['pass']))
-            {
-                try
+                if( 0 === mb_strpos($_POST['user'], 'משתמש_') )
                 {
-                    if( 0 === mb_strpos($_POST['user'], 'משתמש_') )
-                    {
-                        throw new UsernameAlreadyTaken;
-                    }
-                    User::get_current_user()->changeNameAndPass($_POST['user'], $_POST['pass'], 1);
+                    throw new UsernameAlreadyTaken;
                 }
-                catch (UsernameAlreadyTaken $e)
-                {
-                    echo ':err:' , 'שם משתמש זה טפוס';
-                }
+
+                // On failure this one throws UsernameAlreadyTaken either
+                User::get_current_user()->changeNameAndPass($_POST['user'], $_POST['pass'], 1);
             }
-            else
+            catch (UsernameAlreadyTaken $e)
             {
-                echo ':err:' , 'לא הוזנו שם משתמש וסיסמה';
+                echo ':err:' , 'שם משתמש זה טפוס';
             }
         }
+
+    }
 }
