6db05fb6224503ef3c0881f9e8a58b9d12fb76de
Intval <Alex@phpguide.co.il>
2012-01-06 12:45:21 +0200

    urlencode links in recent events

--- protected/components/views/recentEvents.php
+++ protected/components/views/recentEvents.php
@@ -5,10 +5,10 @@
         <div class="event">
             <span><?=e($event['hour']);?></span> <?=e($event['author']);?> <br/>
             <? if($event['is_blog'] === '1'): // blog comment ?>
-            <a href="<?=bu(e($event['location']).".htm#comment".e($event['eventid']));?>"><?=e(urldecode($event['text']));?>&mldr;</a>
+            <a href="<?=bu(urlencode($event['location']).".htm#comment".e($event['eventid']));?>"><?=e(urldecode($event['text']));?>&mldr;</a>
 
             <? elseif($event['is_blog'] === '3'): // forum post ?>
-            <a href="/forum/index.php/topic,<?=e($event['location']);?>.0.html" ><?=e(urldecode($event['text']));?>&mldr;</a>
+            <a href="/forum/index.php/topic,<?=urlencode($event['location']);?>.0.html" ><?=e(urldecode($event['text']));?>&mldr;</a>
             <? endif; ?>
         </div>
         <? endforeach; ?>
