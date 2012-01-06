0c6d45771e250601941b80817e2678e62c685edb
Intval <Alex@phpguide.co.il>
2012-01-06 12:39:06 +0200

    Moved article lists (the list of articles in category and homepage) from //lists dir into //articles, where they belong

--- protected/views/article/articlesInCategory.php
+++ protected/views/article/articlesInCategory.php
@@ -14,10 +14,7 @@
 
 <div id="categories_posts" >
 
-<? foreach ($category->articles as $article):  
-    
-    $article->pub_date = new DateTime($article->pub_date);
-?>
+<? foreach ($category->articles as $article):  ?>
 
     <div class="blogpost">
         
@@ -28,18 +25,18 @@
                     <img src="/static/images/pixel.gif" title="<?=e($article->image);?>" alt="<?=e($article->title);?>" />
                 </div>
                 <div class="right post-content">
-                    <h1 class="title"><a id="post<?=$article->id?>" href="<?=bu(e(urlencode($article->url)));?>.htm" rel="details" title="<?=e($article->description);?>" ><?=e($article->title);?></a></h1>
+                    <h1 class="title"><a id="post<?=$article->id?>" href="<?=bu(e(urlencode($article->url)));?>.htm" rel="bookmark" title="<?=e($article->description);?>" ><?=e($article->title);?></a></h1>
                     <?=$article->html_desc_paragraph;?>
                     <br/>
                     <div class="submited">
-                        <div class="right"><?=e($article->author->full_name);?>, </div>
+                        <div class="right"><?=e($article->author->real_name);?>, </div>
                         <div class="right">&nbsp;<?=Helpers::date2heb($article->pub_date);?></div>
                         <div class="clear"></div>
                     </div>
                 </div>
                 <div class="clear"></div>
             </div>
-            <time datetime="<?=Helpers::date2rfc($article->pub_date);?>" pubdate></time>         
+            <time datetime="<?=Helpers::date2rfc($article->pub_date);?>" pubdate="pubdate"></time>         
         </article>
     </div>
 <?endforeach;?>
