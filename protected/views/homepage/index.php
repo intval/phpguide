e987ea020ee30f18899e4f927b59dca1f5bae070
Intval <Alex@phpguide.co.il>
2012-05-15 20:18:05 +0200

    Fix banner overflowing single blogpost on homepage

--- protected/views/homepage/index.php
+++ protected/views/homepage/index.php
@@ -35,8 +35,9 @@
 </div>
 
 
-
+<div class='homepage-top2-articles'>
 <?  $this->renderPartial('//article/homepageArticlesList', array('articles' =>  array_slice($articles, 0, 2) )); ?>
+</div>
 <div class="homepage-banner">
     פרסם כאן
 </div>
