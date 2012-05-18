21e44ad00ce0b0318459f3a3edc5f02b4cdbd5f6
Intval <Alex@phpguide.co.il>
2012-05-18 09:59:35 +0200

    Incorrect function call instead of procedure in blog_onupdate trigger.

--- protected/data/DB.sql
+++ protected/data/DB.sql
@@ -117,7 +117,7 @@ CREATE TRIGGER `blog_onupdate` AFTER UPDATE ON `blog`
 
 	DECLARE pointsAddition INT(10);
 
-	SET pointsAddition = pointsToAddAfterBlogUpdate( OLD.approved, NEW.approved );
+	CALL pointsToAddAfterBlogUpdate( 0, NEW.approved,  pointsAddition);
 
         IF pointsAddition != 0 THEN BEGIN
 
