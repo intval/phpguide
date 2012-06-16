bc7a1b40415883a458a394494bd867b9c70597c9
Intval <Alex@phpguide.co.il>
2012-06-17 01:36:52 +0200

    fixed appearance direction to rtl in recent events

--- protected/components/views/RecentEvents.php
+++ protected/components/views/RecentEvents.php
@@ -6,11 +6,11 @@
     <dl>
             <dt><?php $this->widget('GravatarWidget', array('size' => 24, 'email' => $comment->CommentAuthor->email)); ?></dt>
             <dd>
-                    <a href="/users/<?=e($comment->CommentAuthor->login);?>" class="nick"><?=e($comment->CommentAuthor->login);?></a>
+                    <a dir='rtl' href="/users/<?=e($comment->CommentAuthor->login);?>" class="nick"><?=e($comment->CommentAuthor->login);?></a>
                     &nbsp;&mdash;&nbsp;
-                    <a href="<?=bu(e(urlencode($comment->Article->url)));?>.htm" class="blog"><?=e($comment->Article->title)?></a>
+                    <a dir='rtl' href="<?=bu(e(urlencode($comment->Article->url)));?>.htm" class="blog"><?=e($comment->Article->title)?></a>
                     &nbsp;&larr;&nbsp; 
-                    <a href="<?=bu(e(urlencode($comment->Article->url)));?>.htm#comment<?=$comment->cid?>" class="topic"><?=e(mb_substr($comment->text,0,100))?></a>
+                    <a dir='rtl' href="<?=bu(e(urlencode($comment->Article->url)));?>.htm#comment<?=$comment->cid?>" class="topic"><?=e(mb_substr($comment->text,0,100, 'utf-8'))?></a>
             </dd>
 
     </dl>
