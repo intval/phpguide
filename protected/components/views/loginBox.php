faf0922dfc53cb5e0770dccb806d630f37febb7d
Intval <Alex@phpguide.co.il>
2012-06-17 17:05:40 +0200

    show points nearby nickname in loginbox

--- protected/components/views/loginBox.php
+++ protected/components/views/loginBox.php
@@ -5,12 +5,20 @@
 <section id="userinfo" class='registered'>
 	<div class="ava">
 		<a href="#" class="avatar"><?php $this->widget('GravatarWidget', array('size' => 48, 'email' => $user->email)); ?></a>
-		<a href="#" class="username"><span dir="ltr" id="user_name"><?=e($user->login)?></span></a>
-		<nav>
-			<a href="#" class='messages' title="הודעות פרטיות"></a>
-			<a href="#" class='favorites' title="מועדפים"></a>
-			<a href="#" class='preferences' title="הגדרות"></a>
-		</nav>
+		<div style="float:right">
+			
+			<div>
+				<a href="<?=bu('users/').urldecode($user->login)?>" class="username"><span dir="ltr" id="user_name"><?=e($user->login)?></span></a> 
+				<span dir="rtl"></span> 
+				<span class='userpoints' title="רייטינג">(<?=$user->points?>)</span>
+			</div>
+			<nav>
+				<a href="#" class='messages' title="הודעות פרטיות"></a>
+				<a href="#" class='favorites' title="מועדפים"></a>
+				<a href="#" class='preferences' title="הגדרות"></a>
+			</nav>
+		</div>
+		<div class='clear'></div>
 	</div>
 	<div class="action">
 		<a href="<?= bu('Add')?>">הוסף פוסט</a>
@@ -25,7 +33,9 @@
 	<div class="ava">
 		<a href="#" class="avatar"><img src='<?=bu('static/images/auth_girl.png')?>' alt='loginbox' /></a>
 		<div style='float:right'>
-			<span dir="ltr" id="user_name"><?=e($user->login)?></span>
+			<span dir="ltr" id="user_name"><?=e($user->login)?></span></a> 
+			<span dir="rtl"></span> 
+			<span class='userpoints' title='רייטינג'>(<?=$user->points?>)</span>
 			<br/>
 			<a href='<?= Yii::app() ->createUrl('login/index'); ?>'>הזדהות והרשמה</a>
 		</div>
