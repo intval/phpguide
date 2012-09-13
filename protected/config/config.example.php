d80c25d71cd88784af3e4a45129f0c7817b85295
Intval <Alex@phpguide.co.il>
2012-09-13 21:39:40 +0200

    separate routes from config

--- protected/config/config.example.php
+++ protected/config/config.example.php
@@ -43,15 +43,7 @@ return array(
                 (
                     'urlFormat'=>'path',
                     'showScriptName'=>false,
-                    'rules'=>array
-                    (
-                        '' => 'homepage/index',
-                    	'q<id:\d+>/<subj:[-_\+\sA-Za-z0-9א-ת]+>.htm' => 'qna/view',
-                        '<article_url:[-_\+\sA-Za-z0-9א-ת]+>\.htm'  => 'Article/index',
-                        'cat/<cat_url:[-_\+\sA-Za-z0-9א-ת]+>\.htm'  => 'Category/index',
-                        '<controller:[a-z]+>/<action:\w+>'          => '<controller>/<action>',
-                        'rss'                                       => 'Homepage/rss'
-                    )
+                    'rules'=> require 'routes.php'
                 ),
             
                 'loid' => array(
