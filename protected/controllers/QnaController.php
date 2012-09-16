a2783d9b42ede993412899aba7ae812a66f31c06
Roman Raslin <roman.raslin@gmail.com>
2012-09-16 15:48:12 +0300

    seperate js file

--- protected/controllers/QnaController.php
+++ protected/controllers/QnaController.php
@@ -124,6 +124,7 @@ class QnaController extends Controller
         	$this->pageAuthor = $qna->author->login;
         	
 	    	$this->addscripts( 'qna','bbcode'); 
+	    	$this->addscripts( 'mark_correct_answer');
 	    	
 	    	if(!static::isQnaViewedEarlier($qna))
 	    	{
--- protected/views/qna/viewQna.php
+++ protected/views/qna/viewQna.php
@@ -30,19 +30,7 @@
 	}
     ?>
 </section>
-<script>
 
-	 $(".correct_ans").click(function() {
-		  aid = $(this).attr('ref');
-		  $(this).addClass('disabled');
-		  $.get("/qna/markascorrect/", { ans: aid } );
-		  $(this).text('תודה לך!');
-
-		  $('.correct_ans[ref!="'+ aid +'"]').remove();
-		  return false;
-		});
-		
-</script>
 
 <?php 
 
