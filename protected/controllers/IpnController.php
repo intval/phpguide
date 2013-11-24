00889c9c0aff38c4cf2afc8688d1ac5ae58d9227
Alex Raskin <Alex@phpguide.co.il>
2013-11-24 15:07:02 +0100

    Integration of paypal ipn step 1

--- protected/controllers/IpnController.php
+++ protected/controllers/IpnController.php
@@ -2,8 +2,12 @@
 
 class IpnController extends PHPGController
 {
+    protected $layout = false;
+
     public function actionIndex()
     {
-
+        $logger = Yii::app()->logger;
+        $ipn = new Ipn($logger, new Helpers(), new IpnListener());
+        $ipn->ProcessIpnRequest($_POST);
     }
 }
---
+++ protected/models/Ipn.php
@@ -0,0 +1,44 @@
+<?php
+
+class Ipn
+{
+    /***
+     * @var CLogger $logger;
+     * @var Helpers $helpers;
+     */
+    protected $logger, $helpers, $ipnListener;
+    private $tempRequestId;
+
+    public function __construct(CLogger $logger, Helpers $emailHelper, IpnListener $ipnListener)
+    {
+        $this->logger = $logger;
+        $this->helpers = $emailHelper;
+        $this->ipnListener = $ipnListener;
+        $this->tempRequestId = rand();
+    }
+
+    public function ProcessIpnRequest(array $postData)
+    {
+        try{
+            $this->logger->log($this->tempRequestId.": Attempting to process ipn request with data: " . var_export($postData, true), CLogger::LEVEL_INFO);
+            $this->TryProcessIpnRequest($postData);
+        }
+        catch(\Exception $ex)
+        {
+            $message = $this->tempRequestId.": Could not process ipn response: \r\n" . $ex->getMessage() . PHP_EOL. PHP_EOL. PHP_EOL . var_export($_REQUEST, true);
+            $this->logger->log($message, CLogger::LEVEL_ERROR);
+            $this->helpers->sendMail([Yii::app()->params['adminEmail']], 'Ipn failure', $message);
+            throw $ex;
+        }
+    }
+
+    private function TryProcessIpnRequest(array $postData)
+    {
+        $this->ipnListener->requirePostMethod();
+        $this->ipnListener->use_sandbox = true;
+        $verified = $this->ipnListener->processIpn($postData);
+
+        $this->logger->log($this->tempRequestId.": Ipn request was verified as <".var_export($verified, true).">;", CLogger::LEVEL_INFO);
+        $this->helpers->sendMail([Yii::app()->params['adminEmail']], 'Succesful purchace', $this->ipnListener->getTextReport());
+    }
+} 
