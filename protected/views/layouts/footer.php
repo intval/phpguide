28b4aebd0a41167a3b2146fecdbbf899ce476831
Alex Raskin <Alex@phpguide.co.il>
2014-07-12 13:54:24 +0200

    using witget for questionnaires

--- protected/views/layouts/footer.php
+++ protected/views/layouts/footer.php
@@ -10,6 +10,9 @@
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular-resource.min.js"></script>
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular-cookies.min.js"></script>
 
+<!-- Witget.com start-->
+<script type="text/javascript"> var gaJsHost = (("https:" == document.location.protocol) ? "https://" : "http://"); document.write(unescape("%3Cscript src='" + gaJsHost + Math.floor(Math.random() * 100)+".loader.witget.com/' type'text/javascript'%3E%3C/script%3E")); </script>
+<!-- Witget.com end-->     
 
 </body>
 </html>
