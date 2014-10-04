5cbe3a93003a9206e8c4b44eb7fef06f84f15799
Alex Raskin <Alex@phpguide.co.il>
2014-10-04 23:21:17 +0400

    Accept any login name as query string parameter (for names with quotes, spaces and dots)

--- protected/config/routes.php
+++ protected/config/routes.php
@@ -13,9 +13,9 @@ return array
 
     ''                                                          => 'homepage/index',
 	'<article_url:[-_\+\sA-Za-z0-9א-ת]+>.htm'                   => 'Article/index',
-	'cat/<cat_url:[-_\+\sA-Za-z0-9א-ת]+>.htm'                  => 'Category/index',
-	'users/<username:[-_\+\sA-Za-z0-9א-ת\.]+>/<action:\w+>'		=> 'Users/<action>',
-	'users/<username:[-_\+\sA-Za-z0-9א-ת\.]+>'		            => 'Users/user',
+	'cat/<cat_url:[-_\+\sA-Za-z0-9א-ת]+>.htm'                   => 'Category/index',
+	'users/<username:[\w\.\'\+]+>/<action:\w+>'		            => 'Users/<action>',
+	'users/<username:[\w\.\'\+]+>'		                        => 'Users/user',
     '<controller:[a-z]+>/<action:\w+>'                          => '<controller>/<action>',
     'rss'                                                       => 'Homepage/rss'
 );
